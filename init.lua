4ebb98d6df3dfa5c7be27851535ae1a2e24c82fe
sriram <lnsriram777@gmail.com>
2023-07-17 19:26:47 +0530

    Commit to change ' rule for toml

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "terafox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -34,6 +34,7 @@ return {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
+          "cpp",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {import = "astrocommunity.colorscheme.gruvbox-baby" },
+  {import = "astrocommunity.colorscheme.nightfox-nvim" },
 }
