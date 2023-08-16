1d9a614a76e1e9dd86b15ebe05e0016e9e9dffa3
sriram <lnsriram777@gmail.com>
2023-08-17 03:22:50 +0530

    Changing the statusline to default one, changing theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,5 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  {import = "astrocommunity.colorscheme.gruvbox-baby" },
-  {import = "astrocommunity.colorscheme.nightfox-nvim" },
+  {import = "astrocommunity.colorscheme.catppuccin"},
 }
